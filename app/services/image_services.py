0d48c5c1d841e68df56ad4b150718f4b4a31fce0
thummanun <thummanunpy@gmail.com>
2025-03-31 21:19:28 +0700

    feat: adjust image processing threshold for improved accuracy; lower from 0.6 to 0.5

--- app/services/image_services.py
+++ app/services/image_services.py
@@ -86,7 +86,7 @@ async def find_similar_faces(event_id: int, file: UploadFile, db: Session):
     try:
         print("Processing Start")
         print("Processing image:", file.filename)
-        threshold = 0.6
+        threshold = 0.5
 
         # อ่านไฟล์เพียงครั้งเดียว
         file_content = await file.read()
